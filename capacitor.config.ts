e25c5c88c181fdff0194adb7f933563ad5fb1b05
Ruben Rafferty <ruben.rafferty@student.umn.ac.id>
2022-02-16 14:33:41 +0700

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'TugasIndividu_36238_RubenRafferty',
+  webDir: 'build',
+  bundledWebRuntime: false
+};
+
+export default config;
